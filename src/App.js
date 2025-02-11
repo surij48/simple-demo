6554d2bcee1e9880f97d27eab46e080a10a6a483
surij48 <surij48@gmail.com>
2025-02-11 15:34:49 +0530

    added env varriable

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
 function App() {
+  console.log("Check env file varriables", process.env.NODE_ENV, process.env.REACT_APP_CUSTOM_NAME)
   return (
     <div className="bg-gray-900 text-white min-h-screen">
       <Header />
