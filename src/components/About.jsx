26a29cfe6c0f4c9431a0be8e726c37dc13b50a2f
surij48 <surij48@gmail.com>
2025-02-11 12:35:19 +0530

    Added yml file

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,10 +3,13 @@ import React from "react";
 function About() {
   return (
     <section className="p-10 text-center bg-gray-800">
-      <h2 className="text-3xl font-bold">About Me:Surendra Jadhav</h2>
+      <h2 className="text-3xl font-bold">About Me:Surendra Jadhav v2</h2>
       <p className="mt-3">I am a passionate full-stack developer with experience in modern web technologies.</p>
     </section>
   );
 }
 
 export default About;
+
+
+// az ad sp create-for-rbac --name "demo-test-v2" --role contributor --scopes /subscriptions/c8a714bf-4d3e-4840-bdda-3724531bd469/resourceGroups/demo-test-v2/providers/Microsoft.Web/sites/demo-test-v2 --json-auth
